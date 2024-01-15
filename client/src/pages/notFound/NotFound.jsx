4a7edcf211719bfc70fada022775f168e42c7920
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-01-15 18:33:26 +0200

    icon and notfound page added

--- client/src/pages/notFound/NotFound.jsx
+++ client/src/pages/notFound/NotFound.jsx
@@ -12,7 +12,7 @@ const NotFound = () => {
         <div className="headerNotFound">
             <button onClick={handleGoHome}>Go Home</button>
         </div>
-        <h3>Opps something went wrong &#128577;</h3>
+        <h3>&#128577; opps something went wrong &#128577;</h3>
         <h1>404 : Page not found</h1>
     </div>
   )
