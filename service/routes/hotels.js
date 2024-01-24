0ace8b1d71096d53c03e80781af36b7018d9ffc5
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-01-24 16:40:53 +0200

    room model added

--- service/routes/hotels.js
+++ service/routes/hotels.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from '../controllers/hotelController.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 //Create
-router.post('/', createHotel);
+router.post('/',verifyAdmin, createHotel);
 //Update
-router.put('/:id', updateHotel);
+router.put('/:id',verifyAdmin, updateHotel);
 //Delete
-router.delete('/:id',deleteHotel);
+router.delete('/:id',verifyAdmin, deleteHotel);
 //Get a hotel
 router.get('/:id', getHotel);
 //Get All
--- service/routes/users.js
+++ service/routes/users.js
@@ -1,21 +1,29 @@
 import express from 'express';
 import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/userController.js';
-import { verifyToken } from '../utils/verifyToken.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 
 const router = express.Router();
 
-router.get("/checkauth", verifyToken, (req,res,next)=>{
-    res.send("Hello user you are loged in")
-})
+// router.get("/checkauth", verifyToken, (req, res, next)=>{
+//     res.send("Hello user you are loged in")
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next)=>{
+//     res.send("Hello user you are loged in and you can delete your account")
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next)=>{
+//     res.send("Hello admin you are loged in and you can delete any account")
+// });
 
 //Update
-router.put('/:id', updateUser);
+router.put('/:id',verifyUser, updateUser);
 //Delete
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyUser, deleteUser);
 //Get a User
-router.get('/:id', getUser);
+router.get('/:id',verifyUser, getUser);
 //Get All
-router.get('/', getAllUsers);
+router.get('/',verifyAdmin, getAllUsers);
 
 export default router;
